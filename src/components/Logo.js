5032d98550b14e6d5298c9fac1bc1ca42ad43fe8
IspahanRahman <ispahanrahman133@gmail.com>
2023-08-03 13:21:11 +0600

    porfolio home page reponsiveness implemented

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -9,12 +9,12 @@ const Logo = () => {
     <div className='flex items-center justify-center mt-2'>
       <MotionLink 
         href="/"
-        className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light'
+        className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light md:w-12 md:h-12 md:text-lg'
         whileHover={{
           backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
           transition:{duration:1, repeat:Infinity}
         }}
-        >CV</MotionLink>
+        >MR</MotionLink>
     </div>
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -46,19 +46,14 @@ const about = () => {
           <div className='grid w-full grid-cols-8 gap-16'>
             <div className='col-span-3 flex flex-col items-start justify-start'>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75 '>Biography</h2>
-              <p className='font-medium'>
-                Hi, I&apos;m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
-                  and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                  new and innovative ways to bring my clients&apos; visions to life.
+              <p className='font-medium text-justify'>
+                Hi, I&apos;m Md. Mahmud-Ur-Rahman,a passionate and dedicated Full-Stack Developer with expertise in Node.js for backend development and React for frontend engineering. I take immense pride in crafting seamless and intuitive digital experiences that leave a lasting impact on users. With a strong focus on code quality and performance, I constantly strive to push the boundaries of what's possible in web development.
               </p>
-              <p className='my-4 font-medium'>
-                I believe that design is about more than just making things look pretty it&apos;s about solving problems and 
-                creating intuitive, enjoyable experiences for users. 
+              <p className='my-4 font-medium text-justify'>
+              I began my journey into the world of software development with a profound fascination for technology.My journey started as a Full-Stack Developer, where I was exposed to the exciting realm of Node.js for backend solutions and React for frontend development.
               </p>
-              <p className='font-medium'>
-                Whether I&apos;m working on a website, mobile app, or 
-                other digital product, I bring my commitment to design excellence and user-centered thinking to 
-                every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+              <p className='font-medium text-justify'>
+                My roles as a Full-Stack Developer allowed me to develop a comprehensive understanding of the end-to-end development process, enabling me to deliver projects with precision and efficiency. 
               </p>
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
